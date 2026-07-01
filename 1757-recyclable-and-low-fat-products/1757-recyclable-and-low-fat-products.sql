ac9435dcd858229a64c37c52e15c60a36409269b
Kirthikalakshmikanthan <krithikalakshmikanthan@gmail.com>
2026-07-01 21:15:43 +0530

    Time: 768 ms (6.17%), Space: 0B (100%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+#create database store;
+#use database store;
+#create table products(
+ #   int product_id,
+  #  enum low_fats,
+   # enum recyclable
+#);
+select product_id from products where low_fats='y' and recyclable='y';
