2466dc423576b1307dbfd54f8c6dc2c5a2d715d8
Kirthikalakshmikanthan <krithikalakshmikanthan@gmail.com>
2026-08-14 22:16:34 +0530

    Time: 0 ms (100%), Space: 45.3 MB (35.06%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -3,16 +3,16 @@ class Solution {
         return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    public boolean validate(TreeNode node, long min, long max) {
-        if (node == null) {
+    public boolean validate(TreeNode root, long min, long max) {
+        if (root == null) {
             return true;
         }
 
-        if (node.val <= min || node.val >= max) {
+        if (root.val <= min || root.val >= max) {
             return false;
         }
 
-        return validate(node.left, min, node.val) &&
-               validate(node.right, node.val, max);
+        return validate(root.left, min, root.val) &&
+               validate(root.right, root.val, max);
     }
 }
