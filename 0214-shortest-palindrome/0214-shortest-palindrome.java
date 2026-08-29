dd96a60596681cd9d7bd5bfcb69b29cdf0581462
Kirthikalakshmikanthan <krithikalakshmikanthan@gmail.com>
2026-08-29 23:31:51 +0530

    Time: 7 ms (81.92%), Space: 47.3 MB (13.66%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        String rev=new StringBuilder(s).reverse().toString();
+        String temp=s+"#"+rev;
+        int[]lps=new int[temp.length()];
+        for(int i=1;i<temp.length();i++){
+            int j=lps[i-1];
+            while(j>0&&temp.charAt(i)!=temp.charAt(j)){
+                j=lps[j-1];
+            }
+            if(temp.charAt(i)==temp.charAt(j)){
+                j++;
+            }
+            lps[i]=j;
+        }
+        int len=lps[temp.length()-1];
+        String remaining=s.substring(len);
+        String revRemaining=new StringBuilder(remaining).reverse().toString();
+        return revRemaining+s;
+    }
+}
